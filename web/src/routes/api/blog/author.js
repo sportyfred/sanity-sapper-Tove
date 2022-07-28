c7aa66700645087f460f8783c2ba44c69256950f
sportyfred <ivanboring@gmail.com>
2022-07-28 18:36:26 +0200

    Create author.js

---
+++ web/src/routes/api/blog/author.js
@@ -0,0 +1,21 @@
+import client from '../../../sanityClient'
+
+export async function get (req, res) {
+  try {
+    // We have access to req.params.slug because the filename has [slug] in it.
+   
+  
+
+    const query = "*[_type == 'author']{_id, slug, title, image, bio}";
+    const author = await client.fetch(query)
+    res.end(JSON.stringify({ author }));
+  } catch (err) {
+    res.writeHead(500, {
+      'Content-Type': 'application/json'
+    });
+
+    res.end(JSON.stringify({
+      message: err.message
+    }));  
+  }
+};
