6e6a40ec238107bc3a69dfa7945eb74a06a60887
sportyfred <ivanboring@gmail.com>
2022-07-28 18:35:15 +0200

    Update about.svelte

--- web/src/routes/about.svelte
+++ web/src/routes/about.svelte
@@ -1,7 +1,54 @@
+<script context="module">
+  export async function preload({ params }) {
+    try {
+      // As with the server route, we have acces to params.slug here
+      const res = await this.fetch(`api/author/author`);
+      const { author } = await res.json();
+      return { author };
+    } catch (err) {
+      this.error(500, err);
+    }
+  };
+</script>
+
+<script>
+  import BlockContent from "@movingbrands/svelte-portable-text";
+  import serializers from "../components/serializers";
+
+  export let author;
+</script>
+
+<style>
+  .content :global(h2) {
+    font-size: 1.4em;
+    font-weight: 500;
+  }
+
+  .content :global(img) {
+    display: block;
+    max-width: 100%;
+  }
+
+  .content :global(figure) {
+    margin: 0;
+  }
+
+  .content :global(ul) {
+    line-height: 1.5;
+  }
+
+  .content :global(li) {
+    margin: 0 0 0.5em 0;
+  }
+</style>
 <svelte:head>
-	<title>About</title>
+  <title>{author[0].name}</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>{author[0].name}</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<div class="content">
+  <BlockContent blocks={author[0].image} {serializers} />
+</div>
+
+ {JSON.stringify(author[0])}
